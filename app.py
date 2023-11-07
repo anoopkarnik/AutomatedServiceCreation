42e0d2fddaf57159c1c53e5b576f90d9c9336409
anoopkarnik <danoopkarnik@gmail.com>
2023-11-08 04:13:49 +0530

    Corrected port number

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ import threading
 import os
 from main.controllers import Controller
 from dotenv import load_dotenv
+import logging
 load_dotenv()
 
 def create_app():
 	app = Flask(__name__)
+	logging.basicConfig(filename='logs/scheduler.log', level=logging.INFO,
+	
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+	app.logger.info('Info level log')
+	app.logger.error('Error level log')
 	app.register_blueprint(Controller.payload_controller)
 	return app
 
